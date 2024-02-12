82beaa65ae3e6b188081ba93e4b1682f8475c503
SagaCSG <158128504+SagaCSG@users.noreply.github.com>
2024-02-12 17:27:55 -0600

    Update app.js

--- adivina el numero cuadro de texto/Nueva carpeta/app.js
+++ adivina el numero cuadro de texto/Nueva carpeta/app.js
@@ -1,30 +1,36 @@
-
-let numeroSecreto = Math.floor (Math.random ()*50)+1;
+//Variables
+let numeroMaximoPosible = 100;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
 let numeroUsuario = 0;
 let intentos = 1;
-//let palabraVeces = "vez"; 
+//let palabraVeces = 'vez';
 let maximosIntentos = 6;
-while(numeroUsuario != numeroSecreto) {
-        numeroUsuario = parseInt(prompt("Me indicas un numero entre 1 y 50 por favor:"));
 
-        console.log(typeof(numeroUsuario));
 
-        if (numeroUsuario == numeroSecreto) {
-            alert(`Acertaste, el numero es : ${numeroUsuario}. lo hiciste en ${intentos} ${intentos == 1 ? "vez" : "veces" }`);
-        //la condicion no se cumplio
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`));
 
+    console.log(typeof(numeroUsuario));
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
         } else {
-            if (numeroUsuario > numeroSecreto){
-            alert ("el numero secreto es menor")
-        } else {
-            alert ("el numero secreto es mayor")
+            alert('El número secreto es mayor');
         }
-             alert("lo siento no acertaste el numero")
-        }
+        //Incrementamos el contador cuando no acierta
+        //intentos = intentos + 1;
+        //intentos += 1;
         intentos++;
-        //palabraVeces = "veces"
-        if (intentos > maximosIntentos ) {
-            alert (`llegaste al numero maximo de ${maximosIntentos} intentos`);
+
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
             break;
         }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
 }
